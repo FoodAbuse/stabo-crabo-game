3d2dfa9d3d9e6f4f183c5a72e295ddba8e52bec0
FoodAbuse <106326154+FoodAbuse@users.noreply.github.com>
2023-10-29 23:30:05 +0800

    Cutscene Cameras, Particles, Shaders and More
    - Fixed Lv2 Camera so it lines up with crab at end of intro
    
    - Created particle system and shader for treasure bucket
    
    - Moved Police Target Area
    
    - Fixed Donut Throw
    
    - +more that I can't remember :///

--- Unity Stuff/Live Project/Stabo Crabo Project/Assets/Scripts/ShaderSwapper.cs
+++ Unity Stuff/Live Project/Stabo Crabo Project/Assets/Scripts/ShaderSwapper.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ShaderSwapper : MonoBehaviour
 {
-    public Shader targetShader;
+    public Shader targetShader1;
+    public Shader targetShader2;
     public Color highlightColour;
     public Color shadowColour;
 
@@ -38,10 +39,21 @@ public class ShaderSwapper : MonoBehaviour
         foreach (Renderer activeObject in activeObjectsInScene)
         {
             //GameObject self = activeObject.gameObject;
-            if (activeObject.material.shader == targetShader)
+            if (activeObject.material.shader == (targetShader1 || targetShader2))
             {
                 //Debug.Log("Found Material");
+                if (activeObject.materials.Length > 1)
+            {
+                Debug.Log("extra material found");
+                int materialCount = activeObject.materials.Length;
 
+                for (int i = 1; i < materialCount; i++)
+                    {
+                        //Debug.Log("updating material no. "+i);
+                        activeObject.materials[i].SetColor("_HighlightColour", highlightColour);
+                        activeObject.materials[i].SetColor("_ShadowColour", shadowColour);                    
+                    }
+            }
                 
                 activeObject.material.SetColor("_HighlightColour", highlightColour);
                 activeObject.material.SetColor("_ShadowColour", shadowColour);
@@ -57,8 +69,9 @@ public class ShaderSwapper : MonoBehaviour
                 {
                     Debug.Log("updating material no. "+i);
                     activeObject.materials[i].SetColor("_HighlightColour", highlightColour);
-                    activeObject.materials[i].SetColor("_ShadowColour", shadowColour);
+                    activeObject.materials[i].SetColor("_ShadowColour", shadowColour);                    
                 }
+                
             }
         }
 
